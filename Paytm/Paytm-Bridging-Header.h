23fcfaa8606b9365fd399b035237311dffda88c2
Apple <mahesh07513@gmail.com>
2019-03-04 18:30:58 +0530

    first commit

---
+++ Paytm/Paytm-Bridging-Header.h
@@ -0,0 +1,16 @@
+//
+//  Paytm-Bridging-Header.h
+//  Paytm
+//
+//  Created by Apple on 01/02/19.
+//  Copyright © 2019 Mahesh. All rights reserved.
+//
+
+#ifndef Paytm_Bridging_Header_h
+#define Paytm_Bridging_Header_h
+
+
+#import "PaymentsSDK.h"
+
+
+#endif /* Paytm_Bridging_Header_h */
--- Paytm/ViewController.swift
+++ Paytm/ViewController.swift
@@ -8,13 +8,41 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,PGTransactionDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    //this function triggers when transaction gets finished
+    func didFinishedResponse(_ controller: ViewController, response responseString: String) {
+        let msg : String = responseString
+        var titlemsg : String = ""
+        if let data = responseString.data(using: String.Encoding.utf8) {
+            do {
+                if let jsonresponse = try JSONSerialization.jsonObject(with: data, options: .mutableContainers) as? [String:Any] , jsonresponse.count > 0{
+                    titlemsg = jsonresponse["STATUS"] as? String ?? ""
+                }
+            } catch {
+                print("Something went wrong")
+            }
+        }
+        let actionSheetController: UIAlertController = UIAlertController(title: titlemsg , message: msg, preferredStyle: .alert)
+        let cancelAction : UIAlertAction = UIAlertAction(title: "OK", style: .cancel) {
+            action -> Void in
+            controller.navigationController?.popViewController(animated: true)
+        }
+        actionSheetController.addAction(cancelAction)
+        self.present(actionSheetController, animated: true, completion: nil)
+    }
+    //this function triggers when transaction gets cancelled
+    func didCancelTrasaction(_ controller : ViewController) {
+        controller.navigationController?.popViewController(animated: true)
+    }
+    //Called when a required parameter is missing.
+    func errorMisssingParameter(_ controller : ViewController, error : NSError?) {
+        controller.navigationController?.popViewController(animated: true)
+    }
 
 }
 
